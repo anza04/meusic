a35df2347aef59b0a173ea1a4a872f28fe18ec1e
anza04 <64538739+anza04@users.noreply.github.com>
2023-12-08 19:51:11 +0100

    upd api calls

--- index.js
+++ index.js
@@ -1,35 +1,53 @@
+/*CONFIGURATION***********************************************************************************************************************************/
+
 const axios = require('axios');
 const express = require('express');
 
 const app = express();
-   
 app.use(express.json());
-
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
 
-app.get("/prova", async(req, res)=>{
-    res.send("connesso");
+app.listen(3000, ()=>{
+  console.log("LISTENING");
 })
 
 
-app.get("/cercaCanzone/", async(req, res) =>{
+/*METHODS*****************************************************************************************************************************************/
+
+app.get("/isUp", async(req, res)=>{
+    res.send("connesso");
+})
+
+app.get("/FindSongByParams/", async(req, res) =>{
     
     const {query, secretKey} = req.body;
 
-    if(secretKey == "14565406587494068402164640684840313064650436")
-        res.send(await cercaCanzone(query))
+    if(CheckKey(secretKey))
+        res.send(await FindSongByParams(query))
     else    
         res.send("You're not allowed to use this method")
 })
 
-async function cercaCanzone(query) {
+app.get("/FindAlbumByParams/", async(req, res) =>{
+    
+  const {query, secretKey} = req.body;
+
+  if(CheckKey(secretKey))
+      res.send(await findAlbumByParams(query))
+  else    
+      res.send("You're not allowed to use this method")
+})
+
+/*FUNCTIONS***************************************************************************************************************************************/
+
+async function FindSongByParams(query) {
     const clientId = 'TuoClientID';
     const clientSecret = 'TuoClientSecret';
-    const token = await ottieniToken(clientId, clientSecret);
+    const token = await getToken(clientId, clientSecret);
 
     const searchUrl = 'https://api.spotify.com/v1/search';
     const response = await axios.get(searchUrl, {
@@ -46,12 +64,37 @@ async function cercaCanzone(query) {
       title: track.name,
       artist: track.artists[0].name,
       spotiLink: track.external_urls.spotify,
-      imgUrl: track.album.images[0].url
+      imgUrl: track.album.images[0].url, 
+      searchUrl: track.href
     }));
   }
 
+async function findAlbumByParams(query) {
+    const clientId = 'TuoClientID';
+    const clientSecret = 'TuoClientSecret';
+    const token = await getToken(clientId, clientSecret);
 
-async function ottieniToken(clientId, clientSecret) {
+    const searchUrl = 'https://api.spotify.com/v1/search';
+    const response = await axios.get(searchUrl, {
+      params: {
+        q: query,
+        type: 'album',
+      },
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return response.data.albums.items.map((album) => ({
+      title: album.name,
+      artist: album.artists[0].name,
+      spotiLink: album.external_urls.spotify,
+      imgUrl: album.images[0].url, 
+      searchUrl: album.href
+    }));
+}
+
+async function getToken(clientId, clientSecret) {
     const tokenUrl = 'https://accounts.spotify.com/api/token';
     const response = await axios.post(
       tokenUrl,
@@ -69,6 +112,11 @@ async function ottieniToken(clientId, clientSecret) {
     return response.data.access_token;
 }
 
-app.listen(3000, ()=>{
-    console.log("LISTENING");
-})
+function CheckKey(key)
+{
+  if(key="14565406587494068402164640684840313064650436")
+    return true
+  else
+    return false
+}
+
